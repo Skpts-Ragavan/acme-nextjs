9a69124e2d51452a688f1efd74b7e550905b59be
Skpts-Ragavan <ragavan@skpts.in>
2024-09-19 07:38:05 -0700

    chapter 12 update action pending

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,20 @@
 'use server';
 
+import { z } from 'zod';
+import { sql } from '@/app/lib/db';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
 export async function createInvoice(formData: FormData) {
     const rawFormData = {
       customerId: formData.get('customerId'),
@@ -7,5 +22,17 @@ export async function createInvoice(formData: FormData) {
       status: formData.get('status'),
     };
     // Test it out:
+
+    const { customerId, amount, status } = CreateInvoice.parse(rawFormData);
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+
+    await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
     console.log(rawFormData);
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
 }
